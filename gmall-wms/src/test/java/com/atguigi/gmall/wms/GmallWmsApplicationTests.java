f65f7a6f6e2093e5aa00f7e76cde2b004ea4fdf0
xiaohuobulai <2672028538.qq.com>
2022-10-02 22:33:14 +0800

    创建文件

---
+++ gmall-wms/src/test/java/com/atguigi/gmall/wms/GmallWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigi.gmall.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallWmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
